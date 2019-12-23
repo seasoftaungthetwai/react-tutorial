ee8fbb5702df8d37ff4717fc017cd2549fbcbcc3
seasoftaungthetwai <seasoft.aung.thetwai@gmail.com>
2019-12-23 09:12:54 +0630

    adding latest update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import {CardList} from './components/card-list/card-list.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters:[] 
+    }
+  }
+  componentDidMount(){
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    // .then(response => response.json())
+    // .then(users => this.setState({monsters:users}));
+    fetch('https://api.pokemontcg.io/v1/cards')
+    .then(response => response.json())
+    .then(users =>this.setState({monsters:users.cards}));
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Pokemontcg</h1>
+        <CardList monsters={this.state.monsters}></CardList>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
